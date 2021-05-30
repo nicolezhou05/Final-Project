15c2acf7f76f57a99bb12336b0d4feb5771dce7c
April Li <78169490+aprilli34@users.noreply.github.com>
2021-05-29 23:36:14 -0400

    Update april.py

--- april.py
+++ april.py
@@ -1,4 +1,20 @@
-def hi():
-  print('hi')
+#Enemy
+enemyImg = pygame.image.load('ogre.png')
+enemyxcord = 300
+enemyycord = 500
+enemyxcordchange = 0 
 
-hi
+#determine if collided
+def collided (monsterx, monstery, playerx, playery):
+    distance = ((monsterx - playerx)**2 + (monstery - playery)**2)**0.5
+    if distance < 54:
+        return True
+    else:
+        return False
+    
+collision = collided(monsterx, monstery, playerx, playery)
+
+# if collision:
+# monster image turn highlighted
+# if player hit monster take damage
+# randomize monster hit or defend 
